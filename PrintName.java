2e8fc78a557fe55162a15643cac952599e51ef90
Anis Zarrad (Computer Science) <a.zarrad@bham.ac.uk>
2024-11-04 14:33:22 +0400

    updated name

--- PrintName.java
+++ PrintName.java
@@ -1,5 +1,5 @@
 public class PrintName {
     public static void main(String[] args) {
-        System.out.println("Your Name"); // Replace "Your Name" with your actual name
+        System.out.println("Anis zarrad"); 
     }
 }
