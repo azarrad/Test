8d084a7209d1097fba42db3bd42771bb1a2068c3
Anis Zarrad (Computer Science) <a.zarrad@bham.ac.uk>
2024-11-08 09:55:01 +0400

    EX2 file new add

--- Ex2.java
+++ Ex2.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World SEPP!");
+        System.out.println("Hello, World 2024!");
     }
 }
